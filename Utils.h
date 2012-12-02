e209c594f6443f63d3fa8a44a828b2fd5156bff9
Vittorio Romeo <vittorio.romeo@outlook.com>
2012-12-02 15:29:03 +0100

    Removed useless toStr

--- Utils.h
+++ Utils.h
@@ -36,14 +36,6 @@ namespace sses
 	{
 		mVector.erase(remove(mVector.begin(), mVector.end(), mItem), mVector.end());
 	}
-
-	template<class T>
-	string toStr(const T &t)
-	{
-		ostringstream oss;
-		oss << t;
-		return string(oss.str());
-	}
 }
 
 #endif /* UTILS_H_SSES */
